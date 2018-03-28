bf4a5ecf16799487e30702cc9e260623ef5f7ef1
daniilkh <daniilkh@debian>
2018-03-28 18:10:39 +0300

    check reordering

--- src/main.cc
+++ src/main.cc
@@ -7,25 +7,45 @@
 #include <utility>
 
 #include "integrals.h"
-#include "davidson.h"
 #include "qm_residue.h"
+
+#define W18 std::setw(18)
 
 namespace pars{
 	const double orto_cutoff = 2E-7;
+	const double saps_printf_cutoff = 0.05;
+	const std::string inpdir = "inp/";
+	const std::string outdir = "out/";
+	const std::string head = " ======== CI-Singles ========";
+};
+
+namespace david_pars{
+	const size_t max_davison_iter = 200;
+	const size_t ntrial_per_state = 2;
+	const size_t trial_space_mult = 4*ntrial_per_state;
+	const size_t ntrial_per_state0 = ntrial_per_state;
+	const double tolerance = 1E-5;
 };
 
 int main(int argc, char *argv[])
 {
 
 	std::string inpfile;
+	std::string outfile;
 	for(size_t i = 0; i < static_cast<size_t>(argc); ++i){
-        if(std::string{argv[i]} == "-inp"){inpfile = std::string{argv[i+1]};}
+        if(std::string{argv[i]} == "-inp"){
+			inpfile = pars::inpdir + std::string{argv[i+1]} + ".inp";
+			outfile = pars::outdir + std::string{argv[i+1]} + ".out";
+		}
     }
 
 	QM_residue mol(inpfile);
-	
+	std::ofstream outf{outfile};
+	outf << pars::head << std::endl;
+	outf.precision(12);
+		
 	Matrix& MOcoef = mol.get_MOs();
-	
+
 	libint2::initialize();
 	
 // check orthonormality of MOs 	
@@ -36,8 +56,7 @@ int main(int argc, char *argv[])
 		Eigen::VectorXd SMOi = S*MOi;
 		auto norm = MOi.dot(SMOi);
 		if(fabs(norm - 1.) > pars::orto_cutoff){
-			std::cout.precision(10);
-			std::cout << "warning: normalization of MO" << i << " = " << norm << std::endl;
+			outf << "warning: normalization of MO" << i << " = " << norm << std::endl;
 		}
 	}	
 	
@@ -57,14 +76,14 @@ int main(int argc, char *argv[])
 	Matrix F = T + V + compute_2body_fock(mol.get_basis(), D);
 
 	auto stop = std::chrono::high_resolution_clock::now();
-	std::cout << "Fock time = " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl;
+	outf << "Fock time = " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl;
 	
 	Eigen::GeneralizedSelfAdjointEigenSolver<Matrix> eigensolver(F, S);
 	auto orbital_e = eigensolver.eigenvalues();
 	
-	std::cout << "orbital energies:" << std::endl;
+	outf << "orbital energies:" << std::endl;
 	for(size_t i = 0; i < mol.nmo; ++i){
-		std::cout << orbital_e[i] << std::endl;
+		outf << orbital_e[i] << std::endl;
 	}
 	
 /*
@@ -122,9 +141,9 @@ int main(int argc, char *argv[])
 	
 	std::sort(saps.begin(), saps.end(), energy_sort);
 
-	std::cout << "SAPS diagonal eneries" << std::endl;
+	outf << "SAPS diagonal eneries" << std::endl;
 	for(const auto& sap : saps){
-		std::cout << sap.first << " -> " << sap.second << "; E = " 
+		outf << sap.first << " -> " << sap.second << "; E = " 
 			<< orbital_e[sap.second] - orbital_e[sap.first] << std::endl;
 	}
 
@@ -133,7 +152,7 @@ int main(int argc, char *argv[])
  *  Davidson procedure	
  */
 
-	size_t nstate = 6;
+	size_t nstate = 2;
 	size_t ntrial = nstate*david_pars::ntrial_per_state; //12;
 	size_t model_dim = ntrial;
 	size_t nvec_used = ntrial;
@@ -169,14 +188,16 @@ int main(int argc, char *argv[])
 		return std::move(Tcis);
 	};
 	
+// old eigenvectors for consistency check	
+	Matrix evec_old;
 	do{
 	
 	std::vector<Matrix> Tcis(ntrial);
 	for(size_t tr = 0; tr < ntrial; ++tr)
 		Tcis[tr] = compute_Tcis(V_trial.col(tr));
 
-//	std::cout << "CIS-S trial density for state 0:" << std::endl;
-//	std::cout << Tcis[0] << std::endl;
+//	outf << "CIS-S trial density for state 0:" << std::endl;
+//	outf << Tcis[0] << std::endl;
 	
 	start = std::chrono::high_resolution_clock::now();
 
@@ -184,10 +205,10 @@ int main(int argc, char *argv[])
 	auto Flike_batch = compute_2body_fock_like_batch_s(mol.get_basis(), Tcis);
 
 	stop = std::chrono::high_resolution_clock::now();
-	std::cout << "Fock-like time = " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl;
+	outf << "Fock-like time = " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << " ms" << std::endl;
 
-//	std::cout << "Fock-like build for trial vector 0:" << std::endl;
-//	std::cout << Flike_batch[0] << std::endl;
+//	outf << "Fock-like build for trial vector 0:" << std::endl;
+//	outf << Flike_batch[0] << std::endl;
 		
 // W_trial = H*V_trial	
 	Matrix W_trial = Matrix::Zero(nsaps, ntrial);
@@ -210,16 +231,32 @@ int main(int argc, char *argv[])
 // projection to model Hamiltonian
 	auto model_H = V_trial.transpose()*W_trial;
 	
-//	std::cout << "Model CI-S Hamiltonian; ntrial = " << ntrial << "; nsaps = " << nsaps << std::endl;
-//	std::cout << model_H << std::endl;
+//	outf << "Model CI-S Hamiltonian; ntrial = " << ntrial << "; nsaps = " << nsaps << std::endl;
+//	outf << model_H << std::endl;
 
 // diagonalization
 	Eigen::SelfAdjointEigenSolver<Matrix> eigensolverH(model_H);
 	
-//	std::cout << "Eigenvalues in a.u.:" << std::endl;	
-//	std::cout << eigensolverH.eigenvalues() << std::endl;
+//	outf << "Eigenvalues in a.u.:" << std::endl;	
+//	outf << eigensolverH.eigenvalues() << std::endl;
+//	outf << eigensolverH.eigenvectors() << std::endl;
 	
 	auto evec = eigensolverH.eigenvectors();
+
+// check consistency of model_H	
+	if(niter > 0){
+		outf << "evec - evec_old residues:" << std::endl;
+		for(size_t tr = 0; tr < ntrial - nstate*david_pars::ntrial_per_state; ++tr){
+			auto res = 0.;
+			for(size_t i = 0; i < ntrial - nstate*david_pars::ntrial_per_state; ++i){
+				res += pow(fabs(evec(i, tr)) - fabs(evec_old(i, tr)), 2.);			
+			}
+			outf << sqrt(res) << std::endl;
+		}
+	}
+	
+	evec_old = evec;
+// end check
 	auto eval = eigensolverH.eigenvalues();
 
 // Ritz vectors	
@@ -228,20 +265,20 @@ int main(int argc, char *argv[])
 		x.col(tr) = V_trial*evec.col(tr);
 	
 // residual vectors
-	std::cout << "Residues:"  << std::endl;
+//	outf << "Residues:"  << std::endl;
 	Matrix res = Matrix::Zero(nsaps, ntrial);
 	for(size_t tr = 0; tr < ntrial; ++tr){
 		res.col(tr) = eval[tr]*x.col(tr) - W_trial*evec.col(tr);
-//		std::cout << res.col(tr).norm() << std::endl;
+//		outf << res.col(tr).norm() << std::endl;
 	}
 	
 // convergence check
-	std::cout << "Iteration " << niter << " :" << std::endl;
-	std::cout << std::setw(16) << "Energy" << std::setw(16) << "Residue" << std::endl;
+	outf << "Iteration " << niter << " :" << std::endl;
+	outf << W18 << "Energy" << W18 << "Residue" << std::endl;
 	bool converged = true;
 	for(size_t i = 0; i < nstate; ++i){
 		auto residue = res.col(i).norm();
-		std::cout << "state " << i + 1 << std::setw(16) << eval[i] << std::setw(16) << residue << std::endl;
+		outf << "state " << i + 1 << W18 << eval[i] << W18 << residue << std::endl;
 		if(residue > david_pars::tolerance) converged = false;
 	}
 	if(converged){
@@ -249,7 +286,7 @@ int main(int argc, char *argv[])
 			cis_eval[i] = eval[i];
 			cis_evec.col(i) = x.col(i);
 		}
-		std::cout << "Davidson converged" << std::endl;
+		outf << "Davidson converged" << std::endl;
 		break;
 	}
 	
@@ -260,19 +297,19 @@ int main(int argc, char *argv[])
 			corr(k, tr) = res(k, tr)/(eval[tr] - Hdiag[k]);
 		}
 	}
-//	std::cout << corr << std::endl;
+//	outf << corr << std::endl;
 	
 	nvec_used += ntrial;
 	assert(nvec_used < nsaps && "number of used trial vectors exceeded number of SAPS");
 		
 	auto ntrial_add = nstate*david_pars::ntrial_per_state;
-	if(ntrial < nstate*david_pars::trial_space_mult - ntrial_add){
+//	if(ntrial <= nstate*david_pars::trial_space_mult - ntrial_add){
 		auto ntrial_old = ntrial;
 		ntrial += ntrial_add;
 		V_trial.conservativeResize(nsaps, ntrial);
 		for(size_t i = 0; i < ntrial_add; ++i)
 			V_trial.col(ntrial_old + i) = corr.col(i);
-	}
+/*	}
 	else{
 		V_trial.resize(nsaps, 2*ntrial_add);
 		ntrial = 2*ntrial_add;
@@ -280,7 +317,7 @@ int main(int argc, char *argv[])
 			V_trial.col(i) = x.col(i);
 		for(size_t i = 0; i < ntrial_add; ++i)
 			V_trial.col(i + ntrial_add) = corr.col(i);
-	}
+	}*/
 		
 	auto QR = V_trial.fullPivHouseholderQr();
 	V_trial = QR.matrixQ().adjoint().topRows(V_trial.cols()).transpose(); 
@@ -290,22 +327,22 @@ int main(int argc, char *argv[])
 	
 	
 	for(size_t i = 0; i < nstate; ++i){
-		std::cout << "State " << i + 1 << std::endl;
-		std::cout << "Energy = " << cis_eval[i] << std::endl;
+		outf << "State " << i + 1 << std::endl;
+		outf << "Energy = " << cis_eval[i] << std::endl;
 		for(size_t k = 0; k < nsaps; ++k){
-			if(fabs(cis_evec(k, i)) > 0.05){
-				std::cout << saps[k].first << " -> " << saps[k].second << ' ' << cis_evec(k, i) << std::endl;
+			if(fabs(cis_evec(k, i)) > pars::saps_printf_cutoff){
+				outf << saps[k].first << " -> " << saps[k].second << ' ' << cis_evec(k, i) << std::endl;
 			}
 		}
 	}
 /*
-	std::cout << "Eigenvectors of model space" << std::endl;
+	outf << "Eigenvectors of model space" << std::endl;
 	for(size_t i = 0; i < ntrial; ++i){
-		std::cout << saps[i].first << " -> " << saps[i].second << ' ';
+		outf << saps[i].first << " -> " << saps[i].second << ' ';
 		for(size_t j = 0; j < ntrial; ++j){
-			std::cout << std::setw(16) << evec(i, j);
+			outf << std::setw(16) << evec(i, j);
 		}
-		std::cout << std::endl;
+		outf << std::endl;
 	}
 */	
 	
